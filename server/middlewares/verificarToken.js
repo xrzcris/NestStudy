67b39a7c99eea1af371dcfdf749bdc1497667f3c
xrzcris <215301839+xrzcris@users.noreply.github.com>
2026-03-20 21:46:46 -0500

    Agregue Verificador de tokens en  middleware para proteger las rutas

---
+++ server/middlewares/verificarToken.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const verificarToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ mensaje: 'Acceso denegado. Token requerido.' });
+  }
+
+  try {
+    const datos = jwt.verify(token, process.env.JWT_SECRET);
+    req.estudiante = datos;
+    next();
+  } catch (error) {
+    return res.status(403).json({ mensaje: 'Token inválido o expirado.' });
+  }
+};
+
+module.exports = verificarToken;
